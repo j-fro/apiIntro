cb0191bd0d99b751e8fa382c57b9b1b0b80b7891
Jacob Froman <jacob.h.froman@gmail.com>
2016-11-30 15:44:16 -0600

    OMDB search and DOM display functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-  console.log('jq ready');
+    console.log('jq ready');
+
+    $('#searchButton').on('click', function() {
+
+        var movieTitle = $('#searchIn').val();
+        // Assemble search URL
+        var searchUrl = 'http://www.omdbapi.com/?s=' + movieTitle;
+        console.log(searchUrl);
+        // Hit the OMDB API with an AJAX call
+        $.ajax({
+            url: searchUrl,
+            dataType: 'JSON',
+            success: function(data) {
+                console.log(data);
+                displayMoviesData(data.Search);
+            }
+        });
+    });
 });
+
+function displayMoviesData(moviesList) {
+  /* Iterates through a list of OMDB movies and adds them to the DOM */
+    var htmlString = '';
+    moviesList.forEach(function(movie) {
+
+        htmlString += '<div><h4>' + movie.Title + '</h4>';
+        htmlString += '<img src="' + movie.Poster + '">';
+        htmlString += '</div>';
+    });
+    $('#moviesDisplay').html(htmlString);
+}
